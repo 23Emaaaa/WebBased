e8b8af0689797d3c1a4633f2cbf5ab66ed87f89e
EmanueleDajko <emanuele.dajko@studio.unibo.it>
2023-11-08 12:01:30 +0100

    Add var_dump for debugging, now is completed

--- TecnologieWeb/EserciziDaSlides/PHP/QuintoTest_Var_Dump.php
+++ TecnologieWeb/EserciziDaSlides/PHP/QuintoTest_Var_Dump.php
@@ -1,5 +1,7 @@
 <?php 
 
-// Per stampare tipo e contenuto di un expr, utile nella fase di debug
+// Per stampare tipo e contenuto di un expr, utile nella fase di debug, devo usare var_dump(<expr>)
 
 $a = array(1, array("a", "b", "c"));
+
+var_dump($a); // print type and content of $a (array)
