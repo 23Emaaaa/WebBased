a6f4ab044890e70825d8938e9e663a566767b074
EmanueleDajko <emanuele.dajko@studio.unibo.it>
2023-11-08 16:20:22 +0100

    Removed PHP files from repository

--- TecnologieWeb/EserciziDaSlides/PHP/QuintoTest_Var_Dump.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-// Per stampare tipo e contenuto di un expr, utile nella fase di debug, devo usare var_dump(<expr>)
-
-$a = array(1, array("a", "b", "c"));
-
-var_dump($a); // print type and content of $a (array)
