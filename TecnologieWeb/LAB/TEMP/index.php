31ae12e3a1f30a2d6add7206d78dc377f53682a6
EmanueleDajko <emanuele.dajko@studio.unibo.it>
2023-11-24 12:57:43 +0100

    Update templateParams variable names

--- TecnologieWeb/LAB/TEMP/index.php
+++ TecnologieWeb/LAB/TEMP/index.php
@@ -4,19 +4,19 @@
 require_once("bootstrap.php");
 
 // Set the title of the page
-$templateparams["titolo"] = "Blog TW - Home";
+$templateParams["titolo"] = "Blog TW - Home";
 
 // Set the name of the template to be used
-$templateparams["nome"] = "lista-articoli.php";
+$templateParams["nome"] = "lista-articoli.php";
 
 // Get 2 random posts from the database
-$templateparams["articolicasuali"] = $dbh->getRandomPosts(2);
+$templateParams["articolicasuali"] = $dbh->getRandomPosts(2);
 
 // Get all categories from the database
-$templateparams["categorie"] = $dbh->getCategories();
+$templateParams["categorie"] = $dbh->getCategories();
 
 // Get the latest 2 posts from the database
-$templateparams["articoli"] = $dbh->getPosts(2);
+$templateParams["articoli"] = $dbh->getPosts(2);
 
 // Include the base template
 require("template/base.php");
